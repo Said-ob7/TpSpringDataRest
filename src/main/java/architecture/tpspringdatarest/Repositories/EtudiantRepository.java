2c4c093859b4812a15a08a56cecba7058a091e9f
Said-ob7 <saideloubid2@gmail.com>
2024-11-04 18:00:00 +0100

    first commit

---
+++ src/main/java/architecture/tpspringdatarest/Repositories/EtudiantRepository.java
@@ -0,0 +1,14 @@
+package architecture.tpspringdatarest.Repositories;
+
+import architecture.tpspringdatarest.Model.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+import java.util.List;
+
+@RepositoryRestResource(path="students")
+public interface EtudiantRepository extends JpaRepository<Etudiant,Integer> {
+    List<Etudiant> findEtudiantByNom(@Param("nom")String nom);
+}
